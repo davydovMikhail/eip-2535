eeabfe1b813622ea3eb8ade7a9072640fae54f90
davydovMikhail <davydov27977@gmail.com>
2022-05-31 17:34:18 +0700

    test2

--- test/diamond.test.js
+++ test/diamond.test.js
@@ -83,11 +83,41 @@ describe('DiamondTest', async function () {
   })
 
   it('should have three facets -- call to facetAddresses function', async () => { // получаем все адреса граней
-    for (const address of await diamondLoupeFacet.facetAddresses()) {
+    for (const address of await diamondLoupeFacet.facetAddresses()) { // получение адресов граней
       addresses.push(address)
     }
 
     assert.equal(addresses.length, 3)
   })
+
+  it('facets should have the right function selectors -- call to facetFunctionSelectors function', async () => {
+    let selectors = getSelectors(diamondCutFacet) // проверка на то, что определенная грань содержит свои функции в виде селекторов
+    result = await diamondLoupeFacet.facetFunctionSelectors(addresses[0])
+    assert.sameMembers(result, selectors)
+    selectors = getSelectors(diamondLoupeFacet)
+    result = await diamondLoupeFacet.facetFunctionSelectors(addresses[1])
+    assert.sameMembers(result, selectors)
+    selectors = getSelectors(ownershipFacet)
+    result = await diamondLoupeFacet.facetFunctionSelectors(addresses[2])
+    assert.sameMembers(result, selectors)
+  })
   
+  it('selectors should be associated to facets correctly -- multiple calls to facetAddress function', async () => {
+    assert.equal(
+      addresses[0],
+      await diamondLoupeFacet.facetAddress('0x1f931c1c') // эта функция возвращает адрес грани, по слектору функции, если эта функция в фасете есть
+    )
+    assert.equal(
+      addresses[1],
+      await diamondLoupeFacet.facetAddress('0xcdffacc6')
+    )
+    assert.equal(
+      addresses[1],
+      await diamondLoupeFacet.facetAddress('0x01ffc9a7')
+    )
+    assert.equal(
+      addresses[2],
+      await diamondLoupeFacet.facetAddress('0xf2fde38b')
+    )
+  })
 })
